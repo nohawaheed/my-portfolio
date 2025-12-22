e5a96cf9e0f10faf21db3178a108c27e34186988
nohawaheed <nohawaheed.1991@gmail.com>
2025-12-22 04:05:23 +0200

    remove slider

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { OwlOptions } from 'ngx-owl-carousel-o';
 import { Projects } from '../projects';
 import { ProjectsJsonApiService } from './../projects-json-api.service';
 import { GsapService } from '../gsap.service';
@@ -12,31 +11,6 @@ import { GsapService } from '../gsap.service';
 export class WorkComponent implements OnInit {
   constructor(private _projectsJsonApiService: ProjectsJsonApiService, private _gsapService: GsapService) { }
   projects: Projects[] = [];
-  customOptions: OwlOptions = {
-    loop: true,
-    autoplay: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: false,
-    dots: false,
-    navSpeed: 700,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      400: {
-        items: 2,
-      },
-      740: {
-        items: 3,
-      },
-      940: {
-        items: 4,
-      },
-    },
-    nav: true,
-    navText: ['<i class="fa fa-arrow-left text-light"></i>', '<i class="fa fa-arrow-right text-light"></i>']
-  };
 
   ngOnInit(): void {
     this._projectsJsonApiService.getProjects().subscribe((response) => {
